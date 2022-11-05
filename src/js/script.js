7aedec719e456956e20f22f171d914b57a50ef7d
Alexander Zinchenko <zinchenko.al1aksandr@yandex.ru>
2022-11-06 01:50:26 +0300

    ready:slick slider adapt

--- src/js/script.js
+++ src/js/script.js
@@ -2,17 +2,15 @@ $(document).ready(function () {
     $('.carousel__inner').slick({
         speed: 1200,
         // adaptiveHeight: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
         prevArrow: '<button type="button" class="slick-prev"><img src="../icons/chevron%20left%20solid.svg" alt="стрелка"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="../icons/chevron%20right%20solid.svg" alt="стрелка"></button>',
         responsive: [
             {
                 breakpoint: 992,
                 settings: {
-                    dots: true,
                     arrows: false,
-                    slidesToShow: 1,
-                    centerMode: true,
-                    variableWidth: true
                 }
             }
         ]
