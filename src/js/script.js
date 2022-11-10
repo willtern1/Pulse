c85a7dc21096cff29932ca3a602aa576a5213242
Alexander Zinchenko <zinchenko.al1aksandr@yandex.ru>
2022-11-11 00:54:30 +0300

    add: validate form + mask

--- src/js/script.js
+++ src/js/script.js
@@ -42,20 +42,53 @@ $(document).ready(function () {
     //открытие окна
     $('[data-modal=consultation]').on('click', function () {
         $('.overlay, #consultation').fadeIn('slow');
-    })
+    });
     // закрытие по кнопке и оверу
     $('.modal__close, .overlay').on('click', function (e) {
         if (e.target === this) {
             $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
         }
-    })
+    });
     //открытие попапа заказа
     $('.button_mini').each(function (i) {
         $(this).on('click', function () {
             $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
             $('.overlay, #order').fadeIn('slow');
         })
-    })
+    });
+    //Валидация
+    function validateForms (form) {
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введи свое имя",
+                    minlength: jQuery.validator.format("Введите более {0}-х символов")
+                },
+                phone: "Пожалуйста, введи свой номер телефона",
+                email: {
+                    required: "Пожалуйста, введите свой E-mail",
+                    email: "Неправильно введен E-mail адресс"
+                }
+            }
+        });
+    }
+
+    validateForms('#consultation-form');
+    validateForms('#order form');
+    validateForms('#consultation form')
+    //Маска для телефоны
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
 });
 
 
